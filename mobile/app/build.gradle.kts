933961bccb62f67f1f71779ba3e39746d2dc3afe
Anonim Uji <anonim.pribadi@contoh.test>
2026-08-23 11:51:40 +0700

    release: v0.4.0

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "ac.undip.sso"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "0.3.3"
+        versionCode = 6
+        versionName = "0.4.0"
     }
 
     signingConfigs {
