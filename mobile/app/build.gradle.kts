c80114a2a0b0523b8fe8ac9ac1c006df056f70ed
test <test@test.com>
2026-09-18 09:31:23 +0700

    chore(mobile): bump version to 0.6.2 (versionCode 12)

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "ac.undip.sso"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "0.6.1"
+        versionCode = 12
+        versionName = "0.6.2"
     }
 
     signingConfigs {
