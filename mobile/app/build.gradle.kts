07fa8cd7cd2ea3d42eb378e1abc7e0e62468c317
test <test@test.com>
2026-09-20 09:26:19 +0700

    chore(mobile): bump version to 0.6.5 (versionCode 15)

--- mobile/app/build.gradle.kts
+++ mobile/app/build.gradle.kts
@@ -39,8 +39,8 @@ android {
         applicationId = "ac.undip.sso"
         minSdk = 26
         targetSdk = 35
-        versionCode = 14
-        versionName = "0.6.4"
+        versionCode = 15
+        versionName = "0.6.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
